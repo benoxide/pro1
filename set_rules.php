efa9d55a8b36e817baf9516348d5e60e675603a2
benoxide <36146496+benoxide@users.noreply.github.com>
2018-02-06 14:04:53 +0530

    Update set_rules.php

--- set_rules.php
+++ set_rules.php
@@ -5,13 +5,14 @@
  * Date: 11/11/2017
  * Time: 6:16 PM
  */
-$servername = "localhost";
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+//$servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "temporary";
 
 // Creating connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($dbhost, $username, $password, $dbname);
 
 // Checking connection
 if (!$conn) {
