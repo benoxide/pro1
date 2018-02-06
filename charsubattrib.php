2a2f1b458f866f1dabe5b577043917cf0010724b
benoxide <36146496+benoxide@users.noreply.github.com>
2018-02-06 13:55:24 +0530

    Update charsubattrib.php

--- charsubattrib.php
+++ charsubattrib.php
@@ -6,13 +6,14 @@
  * Time: 9:56 AM
  */
 
-$servername = "localhost";
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+//$servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "temporary";
 
 // Creating connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($dbhost, $username, $password, $dbname);
 
 // Checking connection
 if (!$conn)
