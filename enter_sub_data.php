94b2a7538d830d4c6e4b3340fe203a2023e85d47
benoxide <36146496+benoxide@users.noreply.github.com>
2018-02-20 18:28:51 +0530

    Update enter_sub_data.php

--- enter_sub_data.php
+++ enter_sub_data.php
@@ -5,9 +5,10 @@
  * Date: 11/3/2017
  * Time: 7:00 PM
  */
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = getenv("MYSQL_SERVICE_HOST");
+//$servername = "localhost";
+$username = "user";
+$password = "password";
 $dbname = "temporary";
 
 // Creating connection
